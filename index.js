26621d80da9fe72a5bff36a45000083dc5b2f995
Blessing <31315580+blessingsunday1995@users.noreply.github.com>
2022-09-26 21:41:31 +0100

    .

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const countdown = document.querySelector('.countdown');
 
-// Set Launch Date (ms)
+
 const launchDate = new Date('Dec 26, 2022 13:00:00').getTime();
 
-// Update every second
+
 const intvl = setInterval(() => {
-  // Get todays date and time (ms)
+ 
   const now = new Date().getTime();
 
-  // Distance from now and the launch date (ms)
   const distance = launchDate - now;
 
-  // Time calculation
+ 
   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
   const hours = Math.floor(
     (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
@@ -19,7 +18,7 @@ const intvl = setInterval(() => {
   const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display result
+  
   countdown.innerHTML = `
   <div>${days}<span>Days</span></div> 
   <div>${hours}<span>Hours</span></div>
@@ -27,11 +26,11 @@ const intvl = setInterval(() => {
   <div>${seconds}<span>Seconds</span></div>
   `;
 
-  // If launch date is reached
+  
   if (distance < 0) {
-    // Stop countdown
+   
     clearInterval(intvl);
-    // Style and output text
+    
     countdown.style.color = '#17a2b8';
     countdown.innerHTML = 'Launched!';
   }
